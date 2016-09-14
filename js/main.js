2769aa0d20577f4fe798de22d0eb14084a30d467
Ryan Tan <ryan_zhiyou@hotmail.com>
2016-09-15 01:24:41 +1200

    progress

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 // var currentWeather: Weather;
-
 var pageheader = $('#page-header')[0];
 var pagecontainer = $('#page-container')[0];
-
 var clicky = $("#clicky")[0];
-
 clicky.addEventListener("click", function () {
     pageheader.innerHTML = "Just a sec while we analyse get the weather from openweathermap.org :)";
     // getweather(currentWeather);
 });
-
+function changeUI() {
+    pageheader.innerHTML = document.getElementById("city").nodeValue;
+}
 // function changeUI() : void {
 //     pageheader.innerHTML = "Current weather is: " + currentWeather.weather;
 //     img.src = currentWeather.emoji;
 //     img.style.display = "block";
-
 //     refreshbtn.style.display = "inline";
 //     pagecontainer.style.marginTop = "20px";
 // }
-
 // function sendWeatherRequest(callback) : void {
 //     $.ajax({
 //         url: "api.openweathermap.org/data/2.5/weather?q=${cur.city},${cur.country}&APPID=14f7fe6958461d28b690d3cda8948696&units=metric",
@@ -38,7 +35,6 @@ clicky.addEventListener("click", function () {
 //             console.log(error.getAllResponseHeaders());
 //         });
 // }
-
 // class Weather {
 //     weather: string;
 //     city: string;
@@ -48,15 +44,11 @@ clicky.addEventListener("click", function () {
 //         this.country = country;
 //     }
 // }
-
 // var clearDay : Weather = new Weather("clear sky", "http://openweathermap.org/img/w/01d.png");
 // not yet done with all the var
-
 // function getCurrWeather(weather : any) : Weather {
 //     currentWeather = 
 // }
 // not yet done as well
-
 // function init() : void {
-    
-// }
+// } 
